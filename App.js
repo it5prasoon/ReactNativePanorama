9248798d429e3949cdf42076084d67df585e65d5
it5prasoon <prasoonk187@gmail.com>
2021-06-13 19:15:55 +0530

    :rocket: added panorama view

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {PanoramaView} from '@lightbase/react-native-panorama-view';
+import {View, Dimensions, StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <View style={styles.container}>
+    <PanoramaView
+      style={styles.viewer}
+      dimensions={{height: 230, width: Dimensions.get('window').width}}
+      inputType="mono"
+      imageUrl="https://raw.githubusercontent.com/googlevr/gvr-android-sdk/master/assets/panoramas/testRoom1_2kMono.jpg"
+    />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  viewer: {
+    height: '100%',
   },
 });
+
+export default App;
